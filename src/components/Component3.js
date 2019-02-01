74a974e60c5377aaa71b503ea2fada98a8227087
Brian Bycynski <high5bri@hotmail.com>
2019-02-01 04:55:04 -0500

    initial commit

---
+++ src/components/Component3.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae commodi
+        nihil assumenda eos perferendis vitae sed exercitationem dolorum
+        quibusdam a ut temporibus possimus explicabo, illum nesciunt animi
+        cupiditate placeat reprehenderit?
+      </p>
+    </div>
+  );
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem 1.5rem 5 rem 1.5rem'
+};
